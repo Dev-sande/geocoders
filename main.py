65e32670a2803d69ad6358e40af68324664ed280
Dev-sande <tapiwasande9@gmail.com>
2023-09-04 15:06:44 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Digital Ground
+
+import geopy.geocoders
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent="geoapiExercises")
+
+place = input("Enter City Name: ")
+
+location = geolocator.geocode(place)
+
+print(location)
+
